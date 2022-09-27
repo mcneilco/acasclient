ec5e76f1eb68d8e23e1d6a669cf3a075e15b33fd
Brian Bolt <brian.bolt@schrodinger.com>
2022-09-27 11:07:53 -0700

    Add project number to example project ls thing for testing

--- tests/project_thing.py
+++ tests/project_thing.py
@@ -15,6 +15,8 @@ BOOLEAN = 'boolean'
 IS_RESTRICTED = 'is restricted'
 RESTRICTED = 'restricted'
 PDF_DOCUMENT = 'pdf document'
+PROJECT_NUMBER_KEY = 'project_number'
+PROJECT_NUMBER = 'project number'
 START_DATE = 'start date'
 NAME_KEY = 'name'
 IS_RESTRICTED_KEY = 'is_restricted'
@@ -32,7 +34,7 @@ class Project(SimpleLsThing):
     ls_kind = PROJECT
     preferred_label_kind = PROJECT_NAME
 
-    def __init__(self, name=None, alias=None, start_date=None, description=None, status=None, is_restricted=True, procedure_document=None, pdf_document=None, recorded_by=None,
+    def __init__(self, name=None, alias=None, start_date=None, description=None, status=None, is_restricted=True, procedure_document=None, pdf_document=None, project_number=None, recorded_by=None,
                  parent_project=None, ls_thing=None):
         names = {PROJECT_NAME: name, PROJECT_ALIAS: alias}
         metadata = {
@@ -43,7 +45,8 @@ class Project(SimpleLsThing):
                 IS_RESTRICTED: CodeValue(str(is_restricted).lower(), BOOLEAN, BOOLEAN, ACAS_DDICT),
                 PROCEDURE_DOCUMENT: BlobValue(file_path=procedure_document),
                 PARENT_PROJECT: CodeValue(parent_project, PROJECT, PROJECT, ACAS_LSTHING),
-                PDF_DOCUMENT: FileValue(file_path=pdf_document)
+                PDF_DOCUMENT: FileValue(file_path=pdf_document),
+                PROJECT_NUMBER: project_number
             }
         }
         super(Project, self).__init__(ls_type=self.ls_type, ls_kind=self.ls_kind, names=names, recorded_by=recorded_by,

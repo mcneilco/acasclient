469dac9d1e3d627467c69300d7d5db4247d60be8
Brian Frost <brian.frost@schrodinger.com>
2022-12-01 10:29:56 -0800

    Bump version: 2022.1.1 → 2022.4.2

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/mcneilco/acasclient',
-    version='2022.1.1',
+    version='2022.4.2',
     zip_safe=False,
 )

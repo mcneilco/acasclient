82d60b2976b8c5cfd79e297506501725878a1f0e
Brian Bolt <brian.bolt@schrodinger.com>
2023-02-01 16:12:47 -0800

    Added type hints

--- acasclient/StereoCategory.py
+++ acasclient/StereoCategory.py
@@ -1,24 +1,24 @@
 class StereoCategory:
-	def __init__(self, id, name, code, version):
+	def __init__(self, id: str, name: str, code: str, version: str) -> None:
 		self.id = id
 		self.name = name
 		self.code = code
 		self.version = version
 
-	def get_id(self):
+	def get_id(self) -> int:
 		return self.id
 
-	def get_name(self):
+	def get_name(self) -> str:
 		return self.name
 
-	def get_code(self):
+	def get_code(self) -> str:
 		return self.code
 
-	def get_version(self):
+	def get_version(self) -> str:
 		return self.version
 
 	# Define as dict for json serialization
-	def as_dict(self):
+	def as_dict(self) -> dict:
 		return {
 			'id': self.id,
 			'name': self.name,
@@ -26,5 +26,5 @@ class StereoCategory:
 			'version': self.version
 		}
 
-	def __str__():
- 		return "id: " + id + " , " + "name: " + name + " , " + "code: " + code + " , " + "version: " + version
+	def __str__(self) -> str:
+		return "id: " + id + " , " + "name: " + name + " , " + "code: " + code + " , " + "version: " + version

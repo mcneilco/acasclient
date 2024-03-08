4c48aa4f0880f3303c6bb40dc62a2c5b3025d69d
Brian Bolt <brian.bolt@schrodinger.com>
2024-03-08 09:00:58 -0800

    Bump version: 2023.3.1 → 2023.3.2

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/mcneilco/acasclient',
-    version='2023.3.1',
+    version='2023.3.2',
     zip_safe=False,
 )

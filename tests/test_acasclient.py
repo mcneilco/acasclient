692702ee1ac2479e3c63e22dec69f6799996f609
Dale Erikson <Dale.Erikson@Schrodinger.com>
2025-04-10 15:31:07 -0400

    Cleanup test

--- tests/test_acasclient.py
+++ tests/test_acasclient.py
@@ -4922,15 +4922,18 @@ class TestExperimentLoader(BaseAcasClientTest):
             }
         ]
         self.check_expected_messages(expected_messages, response['errorMessages'])
-        
+    
     @requires_basic_cmpd_reg_load
     def test_021_experiment_with_custom_metadata(self):
-        """Test experiment loader number with custom experiment metadata"""
+        """Test selfile Generic parser with custom experiment metadata"""
         data_file_to_upload = Path(__file__).resolve()\
             .parent.joinpath('test_acasclient', 'experiment_with_custom_meta_data.csv')
-        response = self.experiment_load_test(data_file_to_upload, False, expect_failure=False)
-        assert response['results']['experimentCode'] is not None
-        experiment = self.client.\
-            get_experiment_by_code(response['results']['experimentCode'], full = True)
+        experiment = Generic()
+        experiment.loadFile(data_file_to_upload)
+        temp_file_path = Path(tempfile.gettempdir()) / f"{experiment.name}.csv"
+        experiment.saveAs(temp_file_path)
+        response = self.experiment_load_test(temp_file_path, False, expect_failure=False)
+        assert response['hasError'] is False
+        experiment = self.client.get_experiment_by_name(experiment.name)
         self.assertIsNotNone(experiment)
         self.assertIn("analysisGroups", experiment)
